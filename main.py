e319467f8a94349bc54571e3c43cf84c7c0a1a4a
Rohitash Singh <93479842+Rohitashsingh89@users.noreply.github.com>
2023-01-22 07:50:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__) 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:@localhost/todo"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://codekunfudatabas:asdfghjklzxcvbnm@codekunfudatabase.mysql.pythonanywhere-services.com/codekunfudatabas$rohitashsingh"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
